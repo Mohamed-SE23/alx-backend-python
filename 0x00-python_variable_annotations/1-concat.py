336bf41efa7db4d8de9cf9108f6867a32d2de70f
Mohamed Alamen <alamenmohamed536@gmail.com>
2024-10-11 09:33:42 +0200

    function to concatinate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module contains function for handling
+two strings and concatinate them together.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns concatinate of two strings.
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: concatinate str1 and str2.
+    """
+    return str1 + str2
--- 0x00-python_variable_annotations/README.md
+++ 0x00-python_variable_annotations/README.md
@@ -1,3 +1,4 @@
 # 0x00 Python Variables Annotations
 0- Basic annotations-add
+1- Basic annotations - concat
 
