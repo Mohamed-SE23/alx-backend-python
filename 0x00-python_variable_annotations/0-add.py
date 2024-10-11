576ae9f1df8cbfc52ea8eaea6f3bc1f6bf7bc865
Mohamed Alamen <alamenmohamed536@gmail.com>
2024-10-11 09:22:16 +0200

    create function to add two numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+this module contains function which
+return the sum of two float numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two numbers.
+
+    Args:
+        a (float): the first number.
+        b (float): the second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,3 @@
+# 0x00 Python Variables Annotations
+0- Basic annotations-add
+
